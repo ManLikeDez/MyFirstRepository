0d0cc991b052a49e3ca33c7bb17b54d03423230d
ManLikeDez <147659899+ManLikeDez@users.noreply.github.com>
2024-09-14 17:22:05 +0100

    Chnages to script

---
+++ Python tests/Area of a Triangle.py
@@ -0,0 +1,11 @@
+# Input the base and height from user
+base = float(input("Enter the length of the base of the traingle: "))
+height =  float(input("Enter the height of the triangle: "))
+
+# Calculate the area of the triangle
+area = 0.5 * base * height
+
+#Display the result
+print(f"The area of the triangle is: {area}")
+
+#END
